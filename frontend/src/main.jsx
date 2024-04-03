3330b67a74e969ada12dbd96ad9d791f03145afd
Akhilbabu456 <akhilbeliever001@gmail.com>
2024-04-04 03:53:46 +0530

    adjusted

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import { NextUIProvider } from '@nextui-org/react'
+
 
 // const customTheme = {
 //   styles: {
@@ -19,14 +19,13 @@ import { NextUIProvider } from '@nextui-org/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
-      <NextUIProvider>
+      
 
     <ChakraProvider >
     <BrowserRouter>
     <App />
     </BrowserRouter>
     </ChakraProvider>
-      </NextUIProvider>
-   
+     
   </React.StrictMode>,
 )
